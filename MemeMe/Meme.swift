891038c51cdfad1727c9a4c3162551415ebb5568
Jeffrey Sulton <notlus68@me.com>
2015-04-12 00:57:33 +0300

    Add a couple of comments

--- MemeMe/Meme.swift
+++ MemeMe/Meme.swift
@@ -8,12 +8,14 @@
 
 import UIKit
 
+/// Represents a single meme
 struct Meme {
     var topText: String
     var bottomText: String
     var sourceImage: UIImage
     var memedImage: UIImage
-    
+
+    /// Intializer for the `Meme` struct
     init(top topText: String, bottom bottomText: String, source sourceImage: UIImage, memed memedImage: UIImage) {
         self.topText = topText
         self.bottomText = bottomText
